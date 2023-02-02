f12286c1da4636bc76d57398e845340eb6864bfd
Onkar <96076073+Onkar731@users.noreply.github.com>
2023-02-02 22:24:20 +0530

    Add files via upload

---
+++ HollowDiamond.java
@@ -0,0 +1,47 @@
+/**
+ Write a java program to print below pattern
+
+                      *                       
+                    *   *                     
+                  *       *                   
+                *           *                 
+              *               *               
+            *                   *             
+          *                       *           
+        *                           *         
+      *                               *       
+    *                                   *     
+  *                                       *   
+*                                           * 
+  *                                       *   
+    *                                   *     
+      *                               *       
+        *                           *         
+          *                       *           
+            *                   *             
+              *               *               
+                *           *                 
+                  *       *                   
+                    *   *                     
+                      *                       
+
+
+ */
+
+
+public class HollowDiamond {
+    public static void main(String[] args) {
+        int n = 23;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i+j == n/2 || j-i == n/2 || i-j == n/2 || i+j == n-1+(n/2)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
